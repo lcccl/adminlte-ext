efc0f4e0c5cac0d911ab1840e8db41fb5fa4e9be
lc <34017828+lcccl@users.noreply.github.com>
2017-11-28 15:56:21 +0800

    更新打包脚本，将File提取出来作为公共的模块。

--- build/build-all.js
+++ build/build-all.js
@@ -1,99 +1,5 @@
-var uglifyJS = require("uglify-js");
-
-/**
- * 文件抽象，封装fs，提供文件操作的常用方法
- */
-var File = function(path, fileName) {
-	this.fs = require("fs");
-	this.path = fileName ? path + "/" + fileName : path;
-	this.fileName = fileName ? fileName : path.substring(path.lastIndexOf("/") + 1);
-	if (this.exists()) {
-		this.stat = this.fs.statSync(this.path);
-	}
-};
-
-File.prototype = {
-	constructor: File,
-
-	getType: function() {
-		return this.path.substring(this.path.lastIndexOf(".") + 1);
-	},
-
-	readText: function(charset) {
-		return this.fs.readFileSync(this.path, charset ? charset : "utf-8");
-	},
-
-	writeText: function(text, charset) {
-		this.fs.writeFileSync(this.path, text, charset ? charset : "utf-8");
-	},
-
-	exists: function() {
-		return this.fs.existsSync(this.path);
-	},
-
-	isFile: function() {
-		return this.stat.isFile();
-	},
-
-	isDir: function() {
-		return this.stat.isDirectory();
-	},
-
-	mkdir: function() {
-		this.fs.mkdirSync(this.path);
-	},
-
-	eachFiles: function(callback) {
-		var me = this;
-		me.fs.readdirSync(me.path).forEach(function(fileName) {
-			var f = new File(me.path, fileName);
-			callback(f);
-		});
-	},
-
-	copyTo: function(destPath, callback) {
-		var me = this;
-
-		if (me.isDir()) {
-			var destDir = new File(destPath);
-			if (!destDir.exists()) {
-				destDir.mkdir();
-			}
-
-			me.eachFiles(function(f) {
-				f.copyTo(destDir.path + "/" + f.fileName, callback);
-			});
-
-			if (callback) {
-				callback(me, destDir);
-			}
-		} else {
-			var readSteam = me.fs.createReadStream(me.path),
-				writeStream = me.fs.createWriteStream(destPath);
-			readSteam.pipe(writeStream);
-
-			writeStream.on("close", function() {
-				if (callback) {
-					callback(me, new File(destPath));
-				}
-			});
-		}
-	},
-
-	delete: function() {
-		var me = this;
-
-		if (me.isDir()) {
-			me.eachFiles(function(file) {
-				file.delete();
-			});
-			me.fs.rmdirSync(me.path);
-		} else {
-			me.fs.unlinkSync(me.path);
-		}
-	}
-};
-
+var uglifyJS = require("uglify-js"),
+	File = require("./file.js");
 
 /*==================== 开始打包，输出到target目录，将adminlte-ext中所有的js、css、img合并 ====================*/
 var target = new File("target");
@@ -130,7 +36,7 @@ packFile(new File("../adminlte-ext"));
 
 // 输出打包后的js文件并压缩
 var jsCode = jsFiles.join("\n");
-// new File("target/adminlte-ext/js/adminlte-ext.js").writeText(jsCode);
+new File("target/adminlte-ext/js/adminlte-ext.js").writeText(jsCode);
 new File("target/adminlte-ext/js/adminlte-ext.min.js").writeText(uglifyJS.minify(jsCode).code);
 // 输出打包后的css文件
 new File("target/adminlte-ext/css/adminlte-ext.css").writeText(cssFiles.join("\n"));
